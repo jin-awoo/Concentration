82947c10cc578412440dc1a5b1396757a17d8719
JINGYA HAN <hjy199411@163.com>
2019-03-19 12:34:13 +1100

    Initial Commit

---
+++ Concentration 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration 2
+//
+//  Created by JINGYA HAN on 19/3/19.
+//  Copyright © 2019 JINGYA HAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
